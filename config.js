b35033daa7c1f41ab3b374c1dc01f82ff3babdad
rafaskis <rafneo@gmail.com>
2026-08-25 10:34:49 +0300

    2025-2027

--- config.js
+++ config.js
@@ -1,11 +1,28 @@
 // Global Configuration for Overtime Tracker
 
 // Allowed navigation year limits
-const MIN_ALLOWED_YEAR = 2026;
-const MAX_ALLOWED_YEAR = 2026;
+const MIN_ALLOWED_YEAR = 2025;
+const MAX_ALLOWED_YEAR = 2027;
 
 // List of holiday dates that should be calculated with Sunday multipliers (YYYY-MM-DD)
 const HOLIDAYS = [
+    "2025-01-01",
+    "2025-01-06",
+    "2025-03-25",
+    "2025-04-01",
+    "2025-05-01",
+    "2025-08-15",
+    "2025-10-01",
+    "2025-10-28",
+    "2025-12-25",
+    "2025-12-26",
+
+    "2025-03-03", // kathara deftera         // kiniti
+    "2025-04-18", // megali paraskevi        // kiniti
+    "2025-04-19", // megalo savvato          // kiniti
+    "2025-04-20", // kiriaki tou pasxa       // kiniti
+    "2025-04-21", // deftera tou pasxa       // kiniti
+
     "2026-01-01",
     "2026-01-06",
     "2026-03-25",
@@ -14,14 +31,29 @@ const HOLIDAYS = [
     "2026-08-15",
     "2026-10-01",
     "2026-10-28",
-    "2026-12-24", // ???
     "2026-12-25",
-    "2026-12-26",  // ???
+    "2026-12-26",
 
     "2026-02-23", // kathara deftera         // kiniti
     "2026-04-10", // megali paraskevi        // kiniti
     "2026-04-11", // megalo savvato          // kiniti
     "2026-04-12", // kiriaki tou pasxa       // kiniti
     "2026-04-13", // deftera tou pasxa       // kiniti
-    "2026-06-01" // deftera agiou pnevmatos  // kiniti
+
+    "2027-01-01",
+    "2027-01-06",
+    "2027-03-25",
+    "2027-04-01",
+    "2027-05-01",
+    "2027-08-15",
+    "2027-10-01",
+    "2027-10-28",
+    "2027-12-25",
+    "2027-12-26",
+
+    "2027-03-15", // kathara deftera         // kiniti
+    "2027-04-30", // megali paraskevi        // kiniti
+    "2027-05-01", // megalo savvato          // kiniti
+    "2027-05-02", // kiriaki tou pasxa       // kiniti
+    "2027-05-03", // deftera tou pasxa       // kiniti
 ];
