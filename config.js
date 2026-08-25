d8e255147379ceb5c4313206dee307f5c17cdcb9
rafaskis <rafneo@gmail.com>
2026-08-25 07:54:52 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+// Global Configuration for Overtime Tracker
+
+// Allowed navigation year limits
+const MIN_ALLOWED_YEAR = 2026;
+const MAX_ALLOWED_YEAR = 2026;
+
+// List of holiday dates that should be calculated with Sunday multipliers (YYYY-MM-DD)
+const HOLIDAYS = [
+    "2026-01-01",
+    "2026-01-06",
+    "2026-03-25",
+    "2026-04-01",
+    "2026-05-01",
+    "2026-08-15",
+    "2026-10-01",
+    "2026-10-28",
+    "2026-12-24", // ???
+    "2026-12-25",
+    "2026-12-26",  // ???
+
+    "2026-02-23", // kathara deftera         // kiniti
+    "2026-04-10", // megali paraskevi        // kiniti
+    "2026-04-11", // megalo savvato          // kiniti
+    "2026-04-12", // kiriaki tou pasxa       // kiniti
+    "2026-04-13", // deftera tou pasxa       // kiniti
+    "2026-06-01" // deftera agiou pnevmatos  // kiniti
+];
